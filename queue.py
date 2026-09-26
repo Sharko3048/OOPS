ef2ec364c5b26ca0cd2d559bcd64abfa5d556384
Sharko3048 <amaya.arora@gmail.com>
2026-09-26 10:55:29 +0200

    Update queue.py

--- queue.py
+++ queue.py
@@ -1,5 +1,41 @@
 class Queue:
     def __init__(self,size):
         self.queue = [None]*size
-l=[1]*4
-print(l)
+        self.front=0
+        self.rear=0
+        self.size=size
+        self.available=size
+    
+    def enqueue(self,value):
+        if self.available == 0:
+            print("Queue overflow")
+        else:
+            self.queue[self.rear] = value
+            self.rear = (self.rear+1) % self.size
+            self.available-=1
+    
+    def dequeue(self):
+        if self.available == self.size:
+            print("Queue underflow")
+        else:
+            self.queue[self.front] = None
+            self.front = (self.front+1) % self.size
+            self.available+=1
+    
+    def peek(self):
+        print(self.queue[self.front])
+    
+    def get_rear(self):
+        print(self.queue[self.rear])
+    
+    def display(self):
+        print(self.queue)
+
+queue1 = Queue(4) 
+queue1.enqueue(10)
+queue1.peek()
+queue1.getRear()
+queue1.enqueue(20)
+queue1.dequeue()
+queue1.peek()
+queue1.print_queue()
